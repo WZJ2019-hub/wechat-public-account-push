e0abcad98deb161f880c1240d51608d861744fd8
WZJ2019-hub <55705809+WZJ2019-hub@users.noreply.github.com>
2022-10-25 22:15:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '周芳芳宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o87cE57UXGfEhch1DIUSZx5kg1a4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KRcB80Q7k6isHHvP6KRA-VFPWiMquWrbggyHule82BI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
